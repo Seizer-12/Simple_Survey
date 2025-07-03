3e1eb9ba57a782358b60cee2fb069b82176483bd
Seizer-12 <idowuenoch65@gmail.com>
2025-07-03 23:26:47 +0100

    Edited configuration for render

--- simple_survey/settings.py
+++ simple_survey/settings.py
@@ -134,3 +134,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MIDDLEWARE.remove('django.contrib.sessions.middleware.SessionMiddleware')
+INSTALLED_APPS.remove('django.contrib.sessions')
